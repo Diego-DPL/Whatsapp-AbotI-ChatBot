863e8d931749eedff2e62a5c495720666b982b5e
Diego-DPL <info@diegodpl.com>
2024-06-25 14:06:23 +0200

    Initial commit

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'AbotI-Chat'
+
+whatsapp_token = 'EAAXZBO68pmm0BO4WlfgIOBIcxptn2LqbrMu5aDeZCikXG3I1mAqZAbVemzASqDZBEJ28D5JsbrXpneoG4H8lg2p8xQ9qD3GvSKklIuOrDWCGjSBl1HWZC3mGBXYXxFZCIIfzP0eOLcVZBTrp0sa6qLIWZBkgKtH98ZCJGWZC7NJkuPuROdxi0hzvDUgmYwQtqx7zZBGDtZBPxJmU5aofqySk'
+
+whatsapp_url = 'https://graph.facebook.com/v19.0/340662112467472/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://drive.google.com/file/d/1ghG6ch_w1wk0QXc6_y9PeQUs-nuASyHO/view?usp=sharing"
